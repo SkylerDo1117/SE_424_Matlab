fe6f377a3efa47c6fefc2ca80fd63dc426b545b2
Do <gsdo2@illinois.edu>
2020-03-05 18:31:06 -0600

    done done, yo. gotta print it tho

--- HW3.m
+++ HW3.m
@@ -1,6 +1,9 @@
 %% SE 424                           Homework 3              Skyler Do, gsdo2
 
 %% I.)
+clc
+clear all
+
 disp("Question I.)")
 P = [0 0 2; 0 2 0; 3 1 2;]
 P_inverse = inv(P);
@@ -15,8 +18,8 @@ v3p = P_inverse * transpose(v3)
 
 
 %% II.)
-clc
-clear all
+%clc
+%clear all
 disp("Question II.)")
 a = [0 1 0; 0 0 1; 8 -12 6];
 b = [2 1 0; 0 2 1; 0 0 2];
@@ -35,8 +38,8 @@ dd = jordan(d)
 
 
 %% III.)
-clc
-clear all
+%clc
+%clear all
 
 disp("Question III.)")
 A = [0 1 0 0; 0 0 1 0; 0 0 0 1; -1 0 -1 0]
@@ -51,8 +54,8 @@ Beig = eig(B)
 
 
 %% IV.)
-clc
-clear all
+%clc
+%clear all
 
 disp("Question IV.)")
 B = [7 -6 -4; -6 8 2; -4 2 3]
@@ -71,8 +74,8 @@ B_third1 = -((-18)*B^2 + 45*B)
 B_third2 = B^3
 
 %% V.)
-clc
-clear all
+%clc
+%clear all
 
 disp("Question V.)")
 A = [2 1 1 0;2 0 0 2]
@@ -80,26 +83,12 @@ A = [2 1 1 0;2 0 0 2]
 disp("Q1")
 range = rank(A)
 disp("Q2")
-null = rank(null(A))
-disp("Qu3")
+nullspace = rank(null(A))
+disp("Q3")
 eig_AtA = eig(transpose(A)*A)
 eig_AAt = eig(A*transpose(A))
 
 %% VI.)
-clc
-clear all
-disp("Question VI.)")
-disp("Q3")
-
-A = [1 2 3; 3 4 6; 7 2 1]
-B = [7 2 1; 7 3 6; 3 4 1]
-
-A_inv = inv(A)
-B_inv = inv(B)
-
-AB_inv = inv((A+B))
-
-
 
 
 
